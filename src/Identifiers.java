4bc8d8a8ad186a416dd526964dbadb701efb6ab9
Raj kishor Das <rajkishordas.04150@gmail.com>
2025-10-24 20:30:17 +0530

    key-word updated

--- src/Identifiers.java
+++ src/Identifiers.java
@@ -8,3 +8,7 @@ public class Identifiers {
 //any name in java called
 //all pre-define class name can also write as identifier but not recommended
 // case-sensitive
+//total reserved word=53
+//reserved literal= 3 (true,false,null)
+//reserverd keyword=50
+//unused keyword= 2 (goto , const )
