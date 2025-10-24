bd45af65184866bb64b87f021f6b096960193acf
Raj kishor Das <rajkishordas.04150@gmail.com>
2025-10-24 19:42:12 +0530

    identifier in java

--- src/Identifiers.java
+++ src/Identifiers.java
@@ -1,7 +1,8 @@
-public class Identifires {
+public class Identifiers {
    public static void main (String[]args){
        int a=12;
        int b=11;
        System.out.println(a+"\n"+b);
    }
 }
+//all pre-define class name can also write as identifier
